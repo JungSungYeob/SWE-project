d43299d4de78c81cb009ce86d0045f46fc2b9432
Sung Yeob Jung <sung87331@gmail.com>
2024-06-06 19:56:44 +0900

    Add HotCoin

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import MenuBar from "../../modules/menuBar/MenuBar"
+import HotCoin from "../../modules/hotCoin/HotCoin";
+
+import styles from "./MainPage.module.css"
+import className from "classnames/bind"
+
+const cx = className.bind(styles)
 
 const MainPage = () =>{
     return(
         <div>
             <MenuBar/>
+            <div className={cx("grid-container")}>
+                <div className={cx("grid-item")}>
+                    <HotCoin/>
+                </div>
+                <div className={cx("grid-item")}></div>
+                <div className={cx("grid-item")}></div>
+                <div className={cx("grid-item")}></div>
+            </div>
         </div>
     )
 }
